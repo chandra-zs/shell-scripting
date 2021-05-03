8b9312504f3bbdfaff78a10373bed00f3297da84
chandra-zs <chandrasekhar@zelarsoft.com>
2021-05-03 17:06:39 +0530

    #1 create component files & common.sh file

--- todo/components/common.sh
+++ todo/components/common.sh
@@ -3,9 +3,22 @@
 HEAD(){
   echo -e "\e[1;36m =============================$1\e[0m"
   echo -e "\e[1;36m =============================$1\e[0m" >>"${LOG}"
-
 }
 
+STAT(){
+  if [ "${1}" -eq 0 ]; then
+    echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILURE\e[0m"
+    echo  -e "\e[1m;31m Pls refer log file for more information, log filepath is =${LOG}\e[0m"
+
+  fi
+}
+
+GIT_CLONE(){
+  git clone "https://github.com/zelar-soft-todoapp/${COMPONENT}.git"
+  cd "${COMPONENT}" || exit
+}
 REPEAT(){
   set-hostname "${COMPONENT}"
   HEAD "Updating apt repos"
--- todo/components/frontend.sh
+++ todo/components/frontend.sh
@@ -3,3 +3,47 @@
 source components/common.sh
 
 REPEAT
+
+HEAD "Install Nginx"
+apt install nginx -y
+STAT $?
+
+HEAD "Start Nginx Service"
+systemctl enable nginx
+systemctl start nginx
+STAT $?
+
+HEAD "Change directory and make todo directory and switch to todo directory"
+cd /var/www/html && mkdir "todo" && cd todo || exit
+STAT $?
+
+HEAD "Install npm  & run build"
+npm install
+npm run build
+STAT $?
+
+HEAD "Change root path in nginx"
+sed -i -e 's/(/var/www/html)/(/var/www/html/todo/frontend/dist' /etc/nginx/sites-available/default
+STAT $?
+
+HEAD "Update index.js File With Todo & Login Ip"
+cd /var/www/html/todo/frontend && cd config || exit
+vi index.js
+sed -i -e
+STAT $?
+
+HEAD "Restart Nginx"
+systemctl restart nginx
+STAT $?
+
+HEAD "run npm start"
+npm start
+STAT $?
+
+
+
+
+
+
+HEAD"Install Npm"
+apt install npm -y

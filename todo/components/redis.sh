edf770482cd57ebfa1521b4423b38177b081b056
chandra-zs <chandrasekhar@zelarsoft.com>
2021-05-05 10:56:16 +0530

    #1 create component files & common.sh file

--- todo/components/redis.sh
+++ todo/components/redis.sh
@@ -5,7 +5,7 @@ source components/common.sh
 REPEAT
 
 HEAD "Install redis server"
-sudo apt install redis-server >>"${LOG}"
+sudo apt install redis-server
 
 HEAD "Change ip address in redis config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf

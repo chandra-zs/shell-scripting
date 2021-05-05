2f21747af842bf796534a48d4f115cb6adc5c04f
chandra-zs <chandrasekhar@zelarsoft.com>
2021-05-05 16:22:39 +0530

    #1 create component files & common.sh file

--- todo/components/users.sh
+++ todo/components/users.sh
@@ -22,13 +22,13 @@ HEAD "Create package"
 mvn clean package >>"${LOG}"
 STAT $?
 
-HEAD "Create jar file"
-java -jar target/users-api-0.0.1.jar >>"${LOG}"
-
-
 HEAD "Create Users Service"
 vi /etc/systemd/system/users.service
 
 HEAD "Start users service"
 systemctl daemon-reload && systemctl start users && systemctl enable users
 STAT $?
+
+HEAD "Create jar file"
+cd users || exit
+java -jar target/users-api-0.0.1.jar >>"${LOG}"

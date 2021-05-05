c4b7a320b0ab564524f100a6e4f5e587e2ecb334
chandra-zs <chandrasekhar@zelarsoft.com>
2021-05-05 21:40:17 +0530

    #1 create component files & common.sh file

--- todo/components/frontend.sh
+++ todo/components/frontend.sh
@@ -44,7 +44,7 @@ STAT $?
 
 HEAD "Change root path in nginx"
 cd /etc/nginx/sites-available || exit
-sed -i 's#/path/to/var/www/html#/path/to/var/www/html/todo/frontend/dist#' default
+sed -i 's#/path/to/var/www/html#/path/to/var/www/html/todo/frontend/dist#g' default
 STAT $?
 
 HEAD "Update index.js File With Todo & Login Ip"

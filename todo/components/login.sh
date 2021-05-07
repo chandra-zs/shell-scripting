719553134604f2438fd79aeecc8aee5be6cfc79f
chandra-zs <chandrasekhar@zelarsoft.com>
2021-05-07 10:39:58 +0530

    #1 create component files & common.sh file

--- todo/components/login.sh
+++ todo/components/login.sh
@@ -4,9 +4,9 @@ source components/common.sh
 
 REPEAT
 
-#Head "Install Go Lang"
-#wget -c https://dl.google.com/go/go1.15.5.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-#STAT $?
+Head "Install Go Lang"
+wget -c https://dl.google.com/go/go1.15.5.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+STAT $?
 
 Head "Set path variables"
 export PATH=$PATH:/usr/local/go/bin

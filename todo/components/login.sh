c4e845871747888145195b7dd35fd13e75619495
chandra-zs <chandrasekhar@zelarsoft.com>
2021-05-11 15:11:41 +0530

    #1 create component files & common.sh file

--- todo/components/login.sh
+++ todo/components/login.sh
@@ -2,27 +2,29 @@
 
 source components/common.sh
 
-Head "Set Hostname and Update Repo"
+HEAD "Set Hostname and Update Repo"
 REPEAT
 STAT $?
 
-Head "Install Go Lang"
+HEAD "Install Go Lang"
 wget -c https://dl.google.com/go/go1.15.5.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
 STAT $?
 
-Head "Set path variables"
+HEAD "Set path variables"
 export PATH=$PATH:/usr/local/go/bin
 source ~/.profile
 go version
 STAT $?
 
-Head "Make directory"
+HEAD "Make directory"
 cd /go && cd src || exit
 STAT $?
+
+HEAD "Clone code"
 GIT_CLONE
 STAT $?
 
-Head "Export go path in directory"
+HEAD "Export go path in directory"
 export GOPATH=/go
 go get github.com/dgrijalva/jwt-go
 go get github.com/labstack/echo
@@ -31,13 +33,15 @@ go get github.com/labstack/gommon/log
 go get github.com/openzipkin/zipkin-go
 go get github.com/openzipkin/zipkin-go/middleware/http
 go get  github.com/openzipkin/zipkin-go/reporter/http
-Head "Build"
+Stat $?
+
+HEAD "Build"
 go build &>>"${LOG}"
 STAT $?
 
-Head "Create login service file"
+HEAD "Create login service file"
 mv systemd.service /etc/systemd/system/login.service
 
-Head "Start login service"
+HEAD "Start login service"
 systemctl daemon-reload && systemctl start login && systemctl status login
 STAT $?

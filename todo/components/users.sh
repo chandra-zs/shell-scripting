f0011c2ff758b8e1c1d520e9e5ea76b9c8761f11
chandra-zs <chandrasekhar@zelarsoft.com>
2021-05-11 11:35:46 +0530

    #1 create component files & common.sh file

--- todo/components/users.sh
+++ todo/components/users.sh
@@ -19,7 +19,7 @@ Head "cleaning the maven package"
 mvn clean package &>>"$LOG"
 
 Head "Now move the user services"
-mv /root/todo/users/users.service /etc/systemd/system/users.service
+mv /root/todo/users/systemd.service /etc/systemd/system/users.service
 
 Head "Restart the services"
 systemctl daemon-reload

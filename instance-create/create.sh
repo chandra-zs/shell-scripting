47dbf82a4f631938246525dafdc1b74cca172162
chandra-zs <chandrasekhar@zelarsoft.com>
2021-07-18 13:21:25 +0530

    #1 create component files & common.sh file

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -25,8 +25,8 @@ Instance_Create() {
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id Z05057823ST8I9KBNU05K --change-batch file:///tmp/record.json
-  sed -i -e "/${COMPONENT}/ d" ../inv
-  echo "${IPADDRESS} COMPONENT=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../inv
+  #sed -i -e "/${COMPONENT}/ d" ../inv
+  #echo "${IPADDRESS} COMPONENT=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../inv
 }
 
 if [ "$1" == "all" ]; then
